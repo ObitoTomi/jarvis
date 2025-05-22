a01407afaf1991f8d267efd1cbdda6c1d7a4f4b7
Timo <timothe.bentaboulet@etu.u-pec.fr>
2025-05-23 01:11:47 +0200

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -1,4 +1,3 @@
-
 import pygame
 import pyaudio
 import numpy as np
@@ -7,6 +6,8 @@ import colorsys
 import win32gui
 import win32con
 import win32api
+
+
 
 # Initialisation de Pygame
 pygame.init()
@@ -41,9 +42,8 @@ while running:
     data = np.frombuffer(stream.read(CHUNK, exception_on_overflow=False), dtype=np.int16)
     spectrum = np.fft.fft(data)  # Transformée de Fourier pour obtenir le spectre
     amplitude = np.max(np.abs(spectrum))  # Intensité du son
-
     # Définition de l'amplitude des vagues (plus y'a de son, plus ça ondule)
-    wave_strength = (amplitude / 8000)  # Ajuster l'intensité des vagues
+    wave_strength = (amplitude / 100000)  # Ajuster l'intensité des vagues
 
     # Changer la couleur dynamiquement
     color_hue += 0.001  # Vitesse de changement de couleur
@@ -55,10 +55,11 @@ while running:
     # Génération des points du cercle déformé
     points = []
     for i in range(num_points):
+
         # Cercle 1
         angle = (i / num_points) * 2 * math.pi  # Angle de chaque point
         # Ondulations dynamiques, nombre de waves (8) et vitesse de rotation pygame.time.get_ticks() * 0.005 et apparation des waves
-        noise = wave_strength * math.sin(8 * angle + pygame.time.get_ticks() * 0.001)  # + pygame.time.get_ticks() * 0.001
+        noise = wave_strength * math.sin(8 * angle + pygame.time.get_ticks() * 0.001)
         radius = base_radius + noise  # Modifier le rayon avec le bruit
         x = WIDTH // 2 + int(radius * math.cos(angle))
         y = HEIGHT // 2 + int(radius * math.sin(angle))
@@ -66,7 +67,7 @@ while running:
 
         # Cercle 2
         angle = (i / num_points) * 2 * math.pi
-        noise = wave_strength * math.sin(12 * angle + pygame.time.get_ticks() * 0.005)
+        noise = wave_strength * math.sin(12 * angle + pygame.time.get_ticks() * 0.001)
         radius = base_radius + noise
         x = WIDTH // 2 + int(radius * math.cos(angle))
         y = HEIGHT // 2 + int(radius * math.sin(angle))
@@ -74,16 +75,14 @@ while running:
 
         # Cercle 3
         angle = (i / num_points) * 2 * math.pi
-        noise = wave_strength * math.sin(6 * angle - pygame.time.get_ticks() * 0.001) #+ pygame.time.get_ticks() * 0.001
+        noise = wave_strength * math.sin(6 * angle - pygame.time.get_ticks() * 0.001) 
         radius = base_radius + noise
         x = WIDTH // 2 + int(radius * math.cos(angle))
         y = HEIGHT // 2 + int(radius * math.sin(angle))
         points.append((x, y))
 
-    # Dessiner le cercle déformé
     pygame.draw.polygon(screen, color, points, 2)
 
-    # In the event loop, add:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -98,3 +97,4 @@ while running:
     clock.tick(60)  
 
 pygame.quit()
+
